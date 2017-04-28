efc888c1554ebc24e25f188dee76a07db588b52c
priom <priom.galaxy@gmail.com>
2017-04-27 23:40:08 -0400

    devise confirmable added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,5 +52,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
